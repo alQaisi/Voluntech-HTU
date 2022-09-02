9ef5f28686bc6305b5afc08893cc68ee97aeb247
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-09-02 23:28:23 +0300

    fixing env variables

--- src/utils/supabase.utils.js
+++ src/utils/supabase.utils.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
-    process.env.API_URL,
-    process.env.API_KEY
+    process.env.REACT_APP_API_URL,
+    process.env.REACT_APP_API_KEY
 );
 export async function signIn({email,password}){
     const { user, session, error } = await supabase.auth.signIn({
